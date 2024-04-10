6f2c30079a0fdc32c0638ca2d0bd33c2e5255e70
GoeunLee <goeun6018@naver.com>
2024-04-10 23:16:26 +0900

    광물 캐기.cpp

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.cpp
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.cpp
@@ -1,16 +1,76 @@
-// #include <string>
-// #include <vector>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
 
-// using namespace std;
+map<string, int> mineral_idx; // <광물들 이름, 이름을 0,1,2로 설정>
+int tired[3][3] = {{1,1,1}, {5,1,1}, {25,5,1}}; // 각 곡괭이별 피로도
+int mineral_size; // 미네랄 리스트 개수
+vector<int> mineral_int_list; // 주어진 minerals를 0,1,2로 변경(mineral_idx)하여 다시 저장
 
-// int solution(vector<int> picks, vector<string> minerals) {
-//     // 최소한의 피로도
-//     // picks: dia, iron, stone 순서로
+int answer = 1250; // 될 수 있는 최고피로도: 25*50
+
+void DFS(vector<int> &picks, int cur, int tire_sum){ // cur:현재위치, tire_sum: 현재까지 쌓인 피로도
     
-//     // 다이아 -> 철 -> 돌 곡괭이 순서로 캐는게 가장 최선의 방법 아닌가..?
-//     // 반례: 피로도가 많이 드는 광물이 뒤에 위치한 경우
+    int pick_tire[3]={0,}; // 곡괭이별 피로도 저장
+    int save_point=0; 
     
-//     int cnt=0; // 광물 캔 횟수
+    if(mineral_size<=cur || picks[0]+picks[1]+picks[2]==0){
+        // 광물들을 다 캤거나, 모든 곡괭이가 더이상 남아있지 않다면 종료
+        answer = min(answer, tire_sum);
+        return;
+    }
+    
+    for(int i=cur;i<cur+5;i++){ // 한 곡괭이별 광물 5개씩
+        if(i>=mineral_size){ // 더이상 광물이 없으면
+            save_point=i;
+            break;
+        }
+        
+        // 각 곡괭이별 피로도 저장
+        pick_tire[0] += tired[0][mineral_int_list[i]];
+        pick_tire[1] += tired[1][mineral_int_list[i]];
+        pick_tire[2] += tired[2][mineral_int_list[i]];
+        
+        save_point=i;
+    }
+    
+    for(int i=0;i<=2;i++){
+        if(picks[i] != 0){
+            picks[i] -= 1;
+            DFS(picks, save_point+1, tire_sum+pick_tire[i]);
+            picks[i] += 1; // 다음 케이스를 위해서
+        }
+    }    
+}
+
+
+int solution(vector<int> picks, vector<string> minerals) {
+    // 최소한의 피로도
+    // picks: dia, iron, stone 순서로
+    
+    // 다이아 -> 철 -> 돌 곡괭이 순서로 캐는게 가장 최선의 방법 아닌가..?
+    // 반례: 피로도가 많이 드는 광물이 뒤에 위치한 경우
+    
+    mineral_idx["diamond"] = 0;
+    mineral_idx["iron"] = 1;
+    mineral_idx["stone"] = 2;
+    
+    mineral_size = minerals.size();
+    
+    for(string name: minerals){
+        mineral_int_list.push_back(mineral_idx[name]);
+    }
+    
+    DFS(picks, 0, 0);
+    
+    return answer;
+    
+    
+    
+// ----------------------------------------------------------    
+//    int cnt=0; // 광물 캔 횟수
 //     int pick_idx=0;
 //     int tired=0; // 피로도
 //     for(int i=0;i<(int)minerals.size();i++){
@@ -54,91 +114,4 @@
 //     }
             
 //     return tired;
-// }
-
-
-#include <string>
-#include <vector>
-#include <map>
-
-#include <iostream>
-using namespace std;
-
-map<string, int>MineralList;                //광물들 이름
-int AllList[3][3]={{1,1,1},{5,1,1},{25,5,1}}; // 각각 피로도
-
-
-vector<int>MineralsIntList;
-
-int ListSize=0;
-int answer = 1250;
-
-//5 개씩 광물을 캔다....
-//곡갱이의 갯수 picks   >> [다이아곡갱이 갯수, 철곡갯수, 돌곡갯수]
-// 광물들의 순서 minerals   >> [순서~~~]
-// 최소의 피로도는?
-
-
-// >>> dfs로 찾기 가능?
-
-void dfs(vector<int> &picks, int location,int sum){ // 곡갱이들, 현재 위치
-    int sumList[3]={0,}; // 피로도 저장
-    int savePoint=0;
-    
-    if(ListSize<=location || picks[0]+picks[1]+picks[2]==0){// 끝까지가거나 곡갱이다씀
-        
-        answer=min(answer,sum);
-        return;
-    }
-    
-    
-    for(int i=location;i<location+5;i++){
-        
-        if(i>=ListSize){
-            savePoint=i;
-            break;
-        }
-        sumList[0]+=AllList[0][MineralsIntList[i]];
-        sumList[1]+=AllList[1][MineralsIntList[i]];
-        sumList[2]+=AllList[2][MineralsIntList[i]];  // 각각의 곡갱이로 일을한것!
-        
-        savePoint=i;
-    }
-    
-    
-    
-    for(int i=0;i<3;i++){//각각 출발
-        if(picks[i]!=0){
-            picks[i]-=1;
-            dfs(picks,savePoint+1,sum+sumList[i]);
-            picks[i]+=1;
-        }
-    }
-    
-    
-}
-
-int solution(vector<int> picks, vector<string> minerals) {
-    
-    
-    MineralList["diamond"]=0; //단어 처리
-    MineralList["iron"]=1;
-    MineralList["stone"]=2;
-    
-    ListSize=minerals.size();
-    
-    //5개씩 나눠야되네!
-    //그럼 5개씩 해서 해결피로도 구하면 되는거 아닌감?
-    
-    
-    for(string i : minerals){
-        MineralsIntList.push_back(MineralList[i]);
-    }
-    
-    
-    
-    dfs(picks,0,0);
-    
-    
-    return answer;
 }
