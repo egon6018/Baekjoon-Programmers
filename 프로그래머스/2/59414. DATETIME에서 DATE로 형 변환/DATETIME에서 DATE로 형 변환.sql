47c89a83233da8d5631999bef32540b515921733
Goeun <goeun6018@naver.com>
2024-07-19 17:52:24 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -0,0 +1,7 @@
+# ANIMAL_INS 테이블에 등록된 모든 레코드에 대해, 
+# 각 동물의 아이디와 이름, 들어온 날짜를 조회하는 SQL문을 작성해주세요. 
+# 이때 결과는 아이디 순으로 조회해야 합니다.
+
+select ANIMAL_ID, NAME, substring(datetime, 1,10) as '날짜'
+from animal_ins
+order by animal_id;
